d1df073ad34dad736531d007e9fa754e1866daa8
AlHassaneDiarra <131254034+AlHassaneDiarra@users.noreply.github.com>
2025-01-31 20:48:36 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabMVC.Models;
+
+namespace LabMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Acceuil()
+        {
+            return View();
+        }
+
+        public ActionResult Commander()
+        {
+            return View();
+        }
+
+        public ActionResult Confirmation(string TxtNom, string TxtPrenom, string TxtDateNaissance, string TxtDescription, string TxtAdresse, string TxtTelephone)
+        {
+            ClientVO clientVO = new ClientVO();
+            ClientDAO clientDAO = new ClientDAO();
+
+
+            clientVO.PrenomClient = TxtPrenom;
+            clientVO.NomClient = TxtNom;
+            clientVO.DateNaissanceClient = TxtDateNaissance;
+            clientVO.DescriptionClient = TxtDescription;
+            clientVO.AdresseClient = TxtAdresse;
+            clientVO.TelephoneClient = TxtTelephone;
+
+            ViewData["numeroconfirmation"] = clientDAO.SauvegardeClient(clientVO);
+            ViewData["prenom"]= TxtPrenom;
+            ViewData["nom"] = TxtNom;
+            ViewData["datenaissance"] = TxtDateNaissance;
+            ViewData["description"] = TxtDescription;
+            ViewData["adresse"] = TxtAdresse;
+            ViewData["telephone"] = TxtTelephone;
+            return View("Confirmation");
+        }
+
+        public ActionResult Produits()
+        {
+            ViewBag.Message = "Liste des produits";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contacter-nous.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/ClientVO.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabMVC.Models
+{
+    class ClientVO
+    {
+        #region Variables et Objets de Cours
+        /// <summary>
+        /// Désigne le numéro du client.
+        /// </summary>
+        private int numeroClient;
+
+
+        private string Prenom;
+        private string Nom;
+        private string DateNaissance;
+        private string Description;
+        private string Adresse;
+        private string Telephone;
+        #endregion
+
+        #region Constructeur de Cours
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoursVO"/> class.
+        /// Constructeur de CoursVO créant un objet ayant les attributs 
+        /// passé en paramètres provenant de CoursDAO - qui accède la base de donnée.
+        /// </summary>
+        public ClientVO()
+        {
+        }
+        #endregion
+
+        #region Accesseurs et Mutateurs pour Cours
+        /// <summary>
+        /// Gets or sets pour numeroCours.
+        /// </summary>
+        public int NumeroClient
+        {
+            get { return this.numeroClient; }
+            set { this.numeroClient = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets pour titreCours.
+        /// </summary>
+        public string NomClient
+        {
+            get { return this.Nom; }
+            set { this.Nom = value; }
+        }
+
+        public string PrenomClient
+        {
+            get { return this.Prenom; }
+            set { this.Prenom = value; }
+        }
+
+        public string DateNaissanceClient
+        {
+            get { return this.DateNaissance; }
+            set { this.DateNaissance = value; }
+        }
+
+        public string DescriptionClient
+        {
+            get { return this.Description; }
+            set { this.Description = value; }
+        }
+
+        public string AdresseClient
+        {
+            get { return this.Adresse; }
+            set { this.Adresse = value; }
+        }
+
+        public string TelephoneClient
+        {
+            get { return this.Telephone; }
+            set { this.Telephone = value; }
+        }
+        #endregion
+    }
+}
